e24e2bebb50b3206adaeec1356285f874b70cdde
Scott Weinstein <sweinstein27@gmail.com>
2018-05-31 13:41:39 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,31 +1,31 @@
 class Triangle
   attr_accessor :a, :b, :c
   def initialize(a, b, c)
-    @a = a 
-    @b = b 
+    @a = a
+    @b = b
     @c = c
   end
 
   def kind
     if valid_triangle?
-      puts true 
-    else 
-      raise TriangleError 
-    end 
-    
+      puts true
+    else
+      raise TriangleError
+    end
+
     if x == y && y == z && x == z
       :equilateral
     elsif x != y && y != z && z != x
-      :scalene 
-    else 
+      :scalene
+    else
       :isosceles
-    end 
+    end
   end
-  
-  def valid_triangle? 
+
+  def valid_triangle?
     x.positive? && y.positive? && z.positive? && x + y > z && x + z > y && y + z > x
-  end 
+  end
 
 class TriangleError < StandardError
-  
+
 end
